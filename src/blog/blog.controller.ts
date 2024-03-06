a0b66b6aaaf17843384dd55d146b73a01c5bf81b
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-03-06 09:46:15 +0200

    changes

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,26 +1,20 @@
 // src/blog/blog.controller.ts
-import { Body, Controller, Get, Post, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guard';
 import { BlogService } from './blog.service';
 import { BlogDto } from './dto/blog.dto';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { CloudinaryService } from './cloudinary.service';
 
 @Controller('blogs')
 export class BlogController {
-  constructor(
-    private readonly blogService: BlogService,
-    private readonly cloudinaryService: CloudinaryService,
-  ) {}
+  constructor(private readonly blogService: BlogService) {}
 
   @UseGuards(JwtGuard)
   @Post()
-  @UseInterceptors(FileInterceptor('image'))
-  async createBlog(@Body() dto: BlogDto, @GetUser() user: User, @UploadedFile() file: Express.Multer.File) {
-    const pictureUrl = await this.cloudinaryService.uploadImage(file);
-    return this.blogService.createBlog({ ...dto, pictureUrl }, user.id);
+  async createBlog(@Body() dto: BlogDto, @GetUser() user: User) {
+    // Assuming the dto has a field 'pictureUrl' for the image URL
+    return this.blogService.createBlog({ ...dto }, user.id);
   }
 
   @Get()
