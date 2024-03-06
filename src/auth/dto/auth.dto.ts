ea93cfe8ce80dfa7a1abceda40e1b4c2d8807899
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-03-06 15:18:11 +0200

    changes

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,16 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString,  } from "class-validator"
 
 export class AuthDto{
+    @IsNotEmpty()
     @IsEmail()
+    email: string;
+  
     @IsNotEmpty()
-    email:string;
     @IsString()
-    @IsNotEmpty()
-    password:string;
-    @IsString()
-    @IsNotEmpty()
-    firstName:string;
-    @IsString()
-    @IsNotEmpty()
-    lastName:string;
+    password: string;
 }
