551c299409f6aa887642e71a0c9c16fc99be7683
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-03-07 15:40:56 +0200

    chamges

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes( new ValidationPipe({
-    whitelist:true
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
   }));
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      '*',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:4200',
+      'https://moiben-blog.vercel.app',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    // credentials: true
+  });
   await app.listen(3333);
 }
 bootstrap();
