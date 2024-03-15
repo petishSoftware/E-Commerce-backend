31da3b8402952260d19acbaf1309f99f2f576270
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-03-15 08:43:31 +0300

    changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ForbiddenException, Injectable, InternalServerErrorException, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
@@ -9,6 +9,7 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private prisma: PrismaService,
     private jwt: JwtService,
@@ -17,28 +18,31 @@ export class AuthService {
 
   async signUp(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-
+  
     try {
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           password: hash,
-          firstName: 'shaphan',
-          lastName: 'kirui',
+          firstName: dto.firstName, // Ensure firstName is included
+          lastName: dto.lastName,
         },
       });
-      delete user.password;
-
+      delete user.password; // Make sure password is not returned in the response
+  
       return this.signToken(user.id, user.email);
     } catch (error) {
+      this.logger.error(`Error occurred during sign-up: ${error.message}`, error.stack);
+      
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials Already Taken');
+          throw new ForbiddenException('Credentials already taken');
         }
       }
-      throw error;
+      throw new InternalServerErrorException('Error occurred during sign-up');
     }
   }
+  
 
   async signIn(dto: AuthDto): Promise<{ access_token: string }> {
     const user = await this.prisma.user.findUnique({
